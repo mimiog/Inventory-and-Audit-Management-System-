37e734bd3809b46968fa6957a94c61f1d42131bd
mstcyr2 <98985878+mstcyr2@users.noreply.github.com>
2022-04-26 00:19:20 -0400

    home page and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { AudHomePageComponent } from './aud-home-page/aud-home-page.component';
+import { DirHomePageComponent } from './dir-home-page/dir-home-page.component';
+import { InventoryPageComponent } from './inventory-page/inventory-page.component';
+import { AuditsPageComponent } from './audits-page/audits-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:AdminHomePageComponent },
+  { path: 'inventory', component:InventoryPageComponent },
+  { path: 'audits', component:AuditsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
