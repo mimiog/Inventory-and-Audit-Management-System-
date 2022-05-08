a14861d51dc3c172449d6cef8a16777ebc4a812a
Anjita Bhandari <abhand3@students.towson.edu>
2022-05-08 02:21:03 -0400

    finally i did something

---
+++ frontend/src/app/edit-items/edit-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-items',
+  templateUrl: './edit-items.component.html',
+  styleUrls: ['./edit-items.component.css']
+})
+export class EditItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
