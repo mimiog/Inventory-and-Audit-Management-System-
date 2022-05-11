f37d436476ac6a6d5ac2ce16f3139c254542935b
Bbiggs1 <bbiggs1@students.towson.edu>
2022-05-11 18:18:18 -0400

    Added text boxes
    added text boxes and correct height and width for the add new item box

--- frontend/src/app/new-items/new-items.component.ts
+++ frontend/src/app/new-items/new-items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 
 @Component({
@@ -9,9 +9,28 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class NewItemsComponent implements OnInit {
 
-  constructor() { }
+  // dialogRef: MatDialogRef<any, any> | undefined ;
+  // constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+    // const dialogRef = this.dialog.open(NewItemsComponent, {
+    //   width: '98vw', //sets width of dialog
+    //   height:'70vh', //sets width of dialog
+    //   maxWidth: '100vw', //overrides default width of dialog
+    //   maxHeight: '100vh', //overrides default height of dialog
+    //   disableClose: true //disables closing on clicking outside box. You will need to make a dedicated button to close
+    // });
   }
+  
+
+//   openDialog() {
+//     this.dialogRef = this.dialog.open(NewItemsComponent, {
+//         height: '40%',
+//         width: '60%'
+//     });
+//     this.dialogRef.afterClosed().subscribe((result: any) => {
+//         this.dialogRef
+//     });
+// }
 
 }
